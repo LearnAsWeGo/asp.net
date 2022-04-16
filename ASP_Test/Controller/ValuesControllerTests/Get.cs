04d427cfcf98b3a9945d401cc298bc1565708ecc
Arush <arush@gust.bz>
2022-04-17 01:58:40 +0530

    test update

---
+++ ASP_Test/Controller/ValuesControllerTests/Get.cs
@@ -0,0 +1,30 @@
+﻿using Jenkins_ASP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASP_Test.Controller.ValuesControllerTests
+{
+    public class Get
+    {
+
+        ValuesController m_controller = new ValuesController();
+
+        public Get()
+        {
+
+        }
+
+        [Fact]
+        public void WhenValid_ReturnsOK()
+        {
+            var response = m_controller.Get();
+
+            Assert.Null(response);
+        }
+    }
+}
