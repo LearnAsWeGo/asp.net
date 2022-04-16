f2e50748856040a0a264795003475a3a4b71177a
Arush <arush@gust.bz>
2022-04-17 00:47:42 +0530

    update publish profile

---
+++ Jenkins_ASP/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jenkins_ASP
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<string> Get()
+        {
+            var response = "Data returned.";
+            return CreatedAtAction(nameof(response), response);
+        }
+    }
+}
